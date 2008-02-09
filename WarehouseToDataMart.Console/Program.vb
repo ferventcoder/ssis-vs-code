24dac955d944c4f44142b120465883bec8c253de
trueblue28 <trueblue28@7901e8e0-ba45-0410-8ced-997fcb3cb136>
2008-02-09 20:31:01 +0000

    Updates to use Trace.WriteLine instead of Console.WriteLine

--- WarehouseToDataMart.Console/Program.vb
+++ WarehouseToDataMart.Console/Program.vb
@@ -5,6 +5,8 @@ Module Program
     Sub Main()
         Dim WHProgram As New WarehouseMoveProgram
         WHProgram.Run()
+        System.Console.WriteLine("Please hit enter to continue...")
+        System.Console.ReadLine()
     End Sub
 
 End Module
